d5643d0b2c9fa69e3e188c0ed507cd4c9697074e
Alessio <avvertix@users.noreply.github.com>
2022-04-05 12:09:26 +0200

    Testing the usage of permissions policy, referrer policy and csp headers (#5)
    * Remove x-powered-by
    
    * Apply secure cookie by default
    
    * Default configuration for session cookies on prod environment
    
    * Testing use of referrer policy, permissions policy and content security

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        env('SESSION_COOKIE_PREFIX', '').Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
     ),
 
     /*
